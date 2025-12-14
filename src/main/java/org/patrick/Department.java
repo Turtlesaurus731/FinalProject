f0346ecb6de4e35028be39b31a77a56805d97b4d
Turtlesaurus731 <patrick.huy.tran@gmail.com>
2025-12-13 20:00:13 -0500

    Complete Department class

--- src/main/java/org/patrick/Department.java
+++ src/main/java/org/patrick/Department.java
@@ -11,4 +11,39 @@ public class Department {
     private String departmentId;
     private String departmentName;
     private static int nextId = 1;
+
+    /**
+     * Check if department name is valid (Only contains letters and spaces)
+     * @param departmentName the name that is to be checked
+     * @return true if valid, otherwise false
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isEmpty()) {
+            return false;
+        }
+        char[] chars = departmentName.toCharArray();
+
+        for (char c : chars) {
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Department(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+            this.departmentId = String.format("d%02d", nextId++);
+        } else {
+            this.departmentName = null;
+            this.departmentId = null;
+        }
+    }
+
+    public void setDepartmentName(String departmentName) {
+        if (isDepartmentNameValid(departmentName)) {
+            this.departmentName = departmentName;
+        }
+    }
 }
